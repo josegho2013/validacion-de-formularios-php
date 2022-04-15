93abe84643157dcba7f239853b4e783a65644a64
jose hernandez <josegho2013@gmail.com>
2022-04-15 12:56:21 -0300

    add model y controller

---
+++ validacion/de/formularios/app/Models/form.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class form extends Model
+{
+    use HasFactory;
+
+    public $fillable=["nombre","email","edad","pass","pass_confirmation"];
+}
---
+++ validacion/de/formularios/database/migrations/2022_04_15_154224_create_forms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->string("nombre");
+            $table->string("email");
+            $table->string("edad");
+            $table->string("pass");
+            $table->string("pass_confirmation");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forms');
+    }
+};
